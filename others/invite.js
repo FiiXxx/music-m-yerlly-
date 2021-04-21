6d8fbeecbbf4ce5851e52c6bfd87ed16310ef8d6
Mooonty <82681184+Mooonty@users.noreply.github.com>
2021-04-22 01:18:50 +0300

    Update invite.js

--- others/invite.js
+++ others/invite.js
@@ -16,7 +16,7 @@ module.exports = {
     message.react("<:Yeh:827947991922376776>");
     //send the invite embed
     message.reply(new MessageEmbed().setColor("#c219d8").setTitle(":Invite KurdMusic: ").setDescription("https://discord.com/api/oauth2/authorize?client_id=832747178295951401&permissions=2188654912&scope=bot")
-    .setFooter("And enjoy listening to music!", "https://cdn.discordapp.com/avatars/769642999227351070/f1b78891507308fb76c0a66b56f4bcd6.webp?size=512"));
+    .setFooter("And enjoy listening to music!", "https://images-ext-1.discordapp.net/external/LmYeWPo2t8r2yUP22ZKyGvLzLBGgLILmYF9mqdGRSD0/https/cdn.discordapp.com/emojis/833038156797575199.png"));
 
   }
 }
