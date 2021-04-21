2a67c23315e0cc89af7c28d467e13831728d512f
Mooonty <82681184+Mooonty@users.noreply.github.com>
2021-04-22 01:15:24 +0300

    Update invite.js

--- others/invite.js
+++ others/invite.js
@@ -13,7 +13,7 @@ module.exports = {
   edesc: "Type this command to get an invite link for the Bot, thanks for every Invite",
   execute(message, args, client) {
     //react with approve emoji
-    message.react("<:VeryBot:832234492872884245>");
+    message.react("<:Yeh:827947991922376776>");
     //send the invite embed
     message.reply(new MessageEmbed().setColor("#c219d8").setTitle(":Invite KurdMusic: ").setDescription("https://discord.com/api/oauth2/authorize?client_id=832747178295951401&permissions=2188654912&scope=bot")
     .setFooter("And enjoy listening to music!", "https://cdn.discordapp.com/avatars/769642999227351070/f1b78891507308fb76c0a66b56f4bcd6.webp?size=512"));
