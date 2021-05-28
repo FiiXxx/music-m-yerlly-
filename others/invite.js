6fe33caf0aee39fb651da38ea158d2026c21b894
Mooonty <82681184+Mooonty@users.noreply.github.com>
2021-05-28 13:28:48 +0300

    Update invite.js

--- others/invite.js
+++ others/invite.js
@@ -15,7 +15,7 @@ module.exports = {
     //react with approve emoji
     message.react("<:Yeh:827947991922376776>");
     //send the invite embed
-    message.reply(new MessageEmbed().setColor("#c219d8").setTitle(":Invite KurdMusic: ").setDescription("https://discord.com/api/oauth2/authorize?client_id=832747178295951401&permissions=2188654912&scope=bot")
+    message.reply(new MessageEmbed().setColor("#c219d8").setTitle(":Invite Fix music: ").setDescription("[invite](https://discord.com/api/oauth2/authorize?client_id=802995353209012234&permissions=2192964928&scope=bot)")
     .setFooter("And enjoy listening to music!", "https://images-ext-1.discordapp.net/external/LmYeWPo2t8r2yUP22ZKyGvLzLBGgLILmYF9mqdGRSD0/https/cdn.discordapp.com/emojis/833038156797575199.png"));
 
   }
