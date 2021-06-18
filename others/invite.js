167421619a5d2770ee71e81911ae9ec7723e4652
Mooonty <82681184+Mooonty@users.noreply.github.com>
2021-06-18 15:22:52 +0300

    Update invite.js

--- others/invite.js
+++ others/invite.js
@@ -13,10 +13,10 @@ module.exports = {
   edesc: "Type this command to get an invite link for the Bot, thanks for every Invite",
   execute(message, args, client) {
     //react with approve emoji
-    message.react("<:Yeh:827947991922376776>");
+    message.react("");
     //send the invite embed
-    message.reply(new MessageEmbed().setColor("BLUE").setTitle("invite fix music").setDescription(" [invite](https://discord.com/api/oauth2/authorize?client_id=802995353209012234&permissions=2192964928&scope=bot)")
-    .setFooter("And enjoy listening to music!", "https://media.discordapp.net/attachments/837136891224522783/848261857919107112/image0.png"));
+    message.reply(new MessageEmbed().setColor("BLUE").setTitle("invite Monty").setDescription(" [invite](https://discord.com/api/oauth2/authorize?client_id=802924076866994186&permissions=7666753&scope=bot)")
+    .setFooter("And enjoy listening to music!", "https://media.discordapp.net/attachments/851570699859656754/855210522421231626/image0.gif"));
 
   }
 }
